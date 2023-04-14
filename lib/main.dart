5025cce1aaf40b061238268fc862059bd11d69b2
Roald Ong <roald.ong@gmail.com>
2023-04-14 17:35:18 +0800

    Added in a navigation rail

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,43 @@ class MyAppState extends ChangeNotifier {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: true,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
+              },
+              ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: Generator(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Generator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
@@ -58,35 +95,33 @@ class MyHomePage extends StatelessWidget {
       icon = Icons.favorite_border_outlined;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(appState: appState),
-            SizedBox(height: 20),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(appState: appState),
+          SizedBox(height: 20),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  print(" like button pressed");
+                  appState.togglefav();
+                },
+                label: Text("Like"),
+                icon: Icon(icon),
+              ),
+              SizedBox(width: 20),
+              ElevatedButton(
                   onPressed: () {
-                    print(" like button pressed");
-                    appState.togglefav();
+                    print("button pressed");
+                    appState.kek();
                   },
-                  label: Text("Like"),
-                  icon: Icon(icon),
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                    onPressed: () {
-                      print("button pressed");
-                      appState.kek();
-                    },
-                    child: Text("wow")),
-              ],
-            )
-          ],
-        ),
+                  child: Text("wow")),
+            ],
+          )
+        ],
       ),
     );
   }
